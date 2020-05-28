c072094fc41c4be5887c4d20044e11816f9bce52
Zeniith <sebastian.binkowski.97@gmail.com>
2020-05-28 16:36:52 +0200

    v1.0.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const items = require("./routes/api/items");
-
+const path = require("path");
 const app = express();
 
 //bodyparser middleware
@@ -22,6 +22,17 @@ mongoose
 
 //use routes
 app.use("/api/items", items);
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started at port: ${port}`));
